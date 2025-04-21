85a56f3567aa94c469af4bf94dc1e6f99e9bdee0
Wuzp <wuzepeng2023@iscas.ac.cn>
2025-04-21 10:29:12 +0800

    Update Daily2145

---
+++ LeetCode/src/DailyProblems/Daily2145.java
@@ -0,0 +1,22 @@
+package DailyProblems;
+
+class Solution2145 {
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+        long min = 0, max = 0;
+        long sum = 0;
+        for (int x : differences) {
+            sum += x;
+            min = Math.min(min, sum);
+            max = Math.max(max, sum);
+        }
+        System.out.println(min + " " + max);
+        return upper - lower >= max - min ? (int)((upper - lower) - (max - min) + 1) : 0;
+    }
+}
+
+public class Daily2145 {
+    public static void main(String[] args) {
+        Solution2145 solution = new Solution2145();
+        System.out.println(solution.numberOfArrays(new int[]{1, 2, 3, 4, 5}, 2, 4));
+    }
+}
